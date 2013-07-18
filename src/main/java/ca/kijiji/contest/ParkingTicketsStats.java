71cb88e9425c4a64a3e0ffd7755aa81def355163
Sam Khan <samk1200@gmail.com>
2013-07-18 15:44:01 -0400

    implementation using regex

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -4,9 +4,14 @@ import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.SortedMap;
+import java.util.regex.Pattern;
 import java.io.BufferedReader;
 
 public class ParkingTicketsStats {
+	
+    static final Pattern number = Pattern.compile("^[\\d\\-+%/!]+ ?");
+    static final Pattern suffix = Pattern.compile(" (S(TREE)?T)?(AVE?)?(RD)?(BLVD)?(COURT)?(CR?T)?$");
+    static final Pattern direction = Pattern.compile(" [NSEW]([EA]ST)?$");
 
     public static HackMap sortStreetsByProfitability(InputStream parkingTicketStream) throws Exception{
       HackMap streetProfitability = new HackMap();
@@ -27,59 +32,15 @@ public class ParkingTicketsStats {
     }
     
     private static String extractStreetName(String address) {
-    	String[] tokens = address.split(" ");
-    	if (tokens.length <= 1) return address;
-    	int last = tokens.length - 1;
-    	int secondLast = last - 1;
-    	tokens[0] = tokens[0].replaceFirst("\\d+-?\\d+", "");
-    	
-    	if (isSuffix(tokens[last])) {
-    		tokens[last] = "";
-    	} else if (isDirection(tokens[last])) {
-    		if (isSuffix(tokens[secondLast])) {
-    			tokens[secondLast] = "";
-    		}
-    		tokens[last] = "";
-    	}
-    	return join(tokens);
-    }
-    
-    private static String join(String[] strings) {
-    	StringBuilder builder = new StringBuilder(strings[0]);
-    	for (String token : strings) {
-    		if (token.length() > 0)
-    			builder.append(' ').append(token);
-    	}
-    	if (builder.toString().length() == 0)
-    		return builder.toString();
-    	return builder.substring(1);
-    }
-    
-    private static boolean isDirection(String token) {
-    	if (token.length() == 1 || token.equals("EAST") || token.equals("WEST")) 
-    		return true;
-    	return false;
-    }
-    
-    private static boolean isSuffix(String token) {
-    	switch (token) {
-	    	case "ST":
-	    	case "STREET":
-	    	case "AVE":
-	    	case "AV":
-	    	case "BLVD":
-	    	case "RD":
-	    	case "COURT":
-	    	case "CRT":
-	    	case "CT":
-	    		return true;
-    	}
-    	return false;
+    	String street = number.matcher(address).replaceFirst("");
+    	String streetName = direction.matcher(street).replaceFirst("");
+    	String finalName = suffix.matcher(streetName).replaceFirst("");
+    	return finalName;
     }
     
     public static void main(String... args) throws Exception {
 	    try ( InputStream ticketReader = new java.io.FileInputStream(args[0]);
-	    		FileWriter writer = new FileWriter(args[1]);
+	    		FileWriter writer = new FileWriter(args[1], true);
 	    ) {
 	    long startTime = System.currentTimeMillis();
 	    SortedMap<String, Integer> hackmap = sortStreetsByProfitability(ticketReader);
